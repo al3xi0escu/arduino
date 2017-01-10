0a9d77d3dd3e85838392bb306d432089227e4878
Alex Ionescu <alexionescu@seoulchristian.org>
2017-01-10 10:41:29 +0900

    Make external led blink

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,12 +1,26 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(13, HIGH);
-  delay(1000);
-  digitalWrite(13, LOW);
-  delay(1000);
+  digitalWrite(12, HIGH); // This starts the led at pin 13
+  delay(1000); // This says how many secs the led should shine
+  digitalWrite(12, LOW); // This stops the led
+  delay(2000); // This says how many secs the led shouldn't shine
+  digitalWrite(12, HIGH); // This starts the led again
+  delay(500); // This this tells the led how many secs it should stay on
+  digitalWrite(12, LOW); // This stops the led again
+  delay(3000); // This says how many secs the led shouldn't shine
+  digitalWrite(12, HIGH); // This starts the led again
+  delay(250); // This says how many secs the led should shine
+  digitalWrite(12, LOW); // This stops the led
+  delay(1000); // This says how many secs the led shouldn't shine
+  digitalWrite(12, HIGH); // This starts the led again
+  delay(1500); // This says how many secs the led should shine
+  digitalWrite(12, LOW); // This stops the led
+  delay(500); // This says how many secs the led shouldn't shine
+  
+
 }
