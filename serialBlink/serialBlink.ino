79c7e08276198ff5d245207f1c7703220c846aed
Alex Ionescu <alexionescu@seoulchristian.org>
2017-01-13 10:22:34 +0900

    Add Serial.readstring variable

--- serialBlink/serialBlink.ino
+++ serialBlink/serialBlink.ino
@@ -1,6 +1,7 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  Serial.setTimeout(10);
   Serial.println("Hello");
   Serial.println("Send 'On' to turn on the LED.");
   Serial.println("Send 'Off' to turn off the LED.");
@@ -9,10 +10,16 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (Serial.readString() == "On") {
-  digitalWrite(13, HIGH);
-  }
-  else if (Serial.readString() == "Off") {
-    digitalWrite(13, LOW);
+  if (Serial.available() > 0) {
+    String command;
+    command = Serial.readString();
+    if (command == "On") {
+      Serial.println("OK, turning the light  on...");
+      digitalWrite(13, HIGH);
+    }
+    else if (command == "Off") {
+      Serial.println("OK, turning the light off...");
+      digitalWrite(13, LOW);
+    }
   }
 }
