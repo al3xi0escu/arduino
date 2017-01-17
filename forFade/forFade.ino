19069496d2cc4e91fcf928ee925d0a210469704d
Alex Ionescu <alexionescu@seoulchristian.org>
2017-01-17 10:32:24 +0900

    Add two for statements to increase/decrease lightValue

--- forFade/forFade.ino
+++ forFade/forFade.ino
@@ -1,10 +1,18 @@
 void setup() {
   Serial.begin(9600);
-  pinMODE(9, OUTPUT);
+  pinMode(9, OUTPUT);
 
 }
 
 void loop() {
-  
-
+  for (int lightValue = 0; lightValue <= 255; lightValue++) {
+    analogWrite(9, lightValue);
+    Serial.println(lightValue);
+    delay(10);
+  }
+  for (int lightValue = 255; lightValue >= 0; lightValue--) {
+    analogWrite(9, lightValue);
+    Serial.println(lightValue);
+    delay(10);
+  }
 }
