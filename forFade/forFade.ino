e33709871c02fb3c2da82ef622c70f8dd36768b4
Alex Ionescu <alexionescu@seoulchristian.org>
2017-01-19 10:44:13 +0900

    Comment on ifFade and forFade

--- forFade/forFade.ino
+++ forFade/forFade.ino
@@ -6,13 +6,13 @@ void setup() {
 
 void loop() {
   for (int lightValue = 0; lightValue <= 255; lightValue++) { // Gradually increase brightness of LED to the maximum value of 255
-    analogWrite(9, lightValue); // This is the pin and value
-    Serial.println(lightValue); // Set output lightValue to LED
+    analogWrite(9, lightValue); // Sends integer to pin 9 in analog form
+    Serial.println(lightValue); // Sends integer to serial monitor
     delay(10); // Says how many milliseconds the LED is on
   }
   for (int lightValue = 255; lightValue >= 0; lightValue--) { // Gradually decrease brightness of LED to the minimum value of 0 
-    analogWrite(9, lightValue); // This is the pin and value
-    Serial.println(lightValue); // Set output lightValue to LED
+    analogWrite(9, lightValue); // Sends integer to pin 9 in analog form
+    Serial.println(lightValue); // Sends integer to serial monitor
     delay(10); // Says how many milliseconds the LED is off
   }
 }
--- ifFade/ifFade.ino
+++ ifFade/ifFade.ino
@@ -1,17 +1,17 @@
-int lightValue = 0;
-int delta = 1;
+int lightValue = 0; // Set value of lightValue to 0
+int delta = 1; // Set value of delta to 1
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(9, OUTPUT);
+  Serial.begin(9600); // Set baud rate to 9600 
+  pinMode(9, OUTPUT); // Sets pin 9 to output
 }
 
 void loop() {
-  lightValue = lightValue + delta;
-  if (lightValue <= 0 || lightValue >= 255) {
-    delta = -delta;
+  lightValue = lightValue + delta; // Add lightValue to delta to make a new lightValue 
+  if (lightValue <= 0 || lightValue >= 255) { // If lightValue is smaller or equal to 0 and greater or equal to 255 line 12 runs
+    delta = -delta; // The variable negates the value of delta
   }
-  analogWrite(9, lightValue);
-  Serial.println(lightValue);
-  delay(10);
+  analogWrite(9, lightValue); // Sends integer to pin 9 in analog form
+  Serial.println(lightValue); // Sends integer to serial monitor
+  delay(10); // The program stos for 10 milliseconds
 }
